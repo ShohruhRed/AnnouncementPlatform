cd1211d1e78989379ddab641e71d3f1235ca3467
Shohruh Abduvohidov <shohruhred@inbox.ru>
2024-01-28 18:50:35 +0500

    Add announcements controller

---
+++ API/Controllers/AnnouncementsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnnouncementsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetAnnouncements()
+        {
+            return "this will be a list of announcements";
+        }
+
+        [HttpGet("{id}")]
+        public string GetAnnounce(int id)
+        {
+            return "singe announce";
+        }
+    }
+}
